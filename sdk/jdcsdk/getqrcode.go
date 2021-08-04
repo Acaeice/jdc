8556683ffd2637c4d4889605e63dbad16199896f
“wuzuo” <wanghejun@meikeland.com>
2021-08-04 17:50:49 +0800

    fix

--- sdk/jdcsdk/getqrcode.go
+++ sdk/jdcsdk/getqrcode.go
@@ -1,10 +1,9 @@
 package jdcsdk
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 )
 
 const (
@@ -18,26 +17,16 @@ type Body struct {
 	Img        string `json:"img"`
 }
 
-func GetQrcode() (*Body, error) {
+func GetQrcode() ([]byte, error) {
 	uri := fmt.Sprintf(getQrcode, host)
-	result := &struct {
-		ResultCode string `json:"resultCode"`
-		ResultMsg  string `json:"resultMsg"`
-	}{}
-	resp, err := client.Net.Get(uri).End(nil, result)
+	resp, err := http.Get(uri)
 	if err != nil {
-		return nil, netError(err)
+		return nil, err
 	}
-	if result.ResultCode != respOk {
-		return nil, errors.New(result.ResultMsg)
-	}
+	// defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
-	resultqrcode := Body{}
-	if err := json.Unmarshal(body, &result); err != nil {
-		return nil, err
-	}
-	return &resultqrcode, nil
+	return body, nil
 }
--- sdk/jdcsdk/getqrcode_test.go
+++ sdk/jdcsdk/getqrcode_test.go
@@ -1,21 +1,27 @@
 package jdcsdk
 
 import (
+	"encoding/json"
 	"fmt"
 	"testing"
 )
 
 const (
-	testhost = "localhost:8080"
+	testhost = "127.0.0.1:8080"
 )
 
 func Test_GetQrcode(t *testing.T) {
 	Init(testhost)
 	res, err := GetQrcode()
+	result := &Body{}
 	if err != nil {
+		fmt.Println("++++++++++++++++++++++++++++++++++++++++")
 		fmt.Println("err:", err)
+		// return
+	}
+	if err := json.Unmarshal(res, &result); err != nil {
 		return
 	}
 	fmt.Println("-----------------------------------------")
-	fmt.Println("res:", res.Img)
+	fmt.Println("res:", result)
 }
